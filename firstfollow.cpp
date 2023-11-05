2ac6f2e773e3720a71617f3d757b2d9494175bc3
Paul Francis <paulumonty@gmail.com>
2023-11-05 21:46:03 +0000

    strings

--- firstfollow.cpp
+++ firstfollow.cpp
@@ -1,2 +1,21 @@
 // We take as input transformedgrammar.txt
 // and then we calculate FIRST for every production and FOLLOW for every non terminal
+
+// we want production class. 
+// list of productions
+// first is a function that takes a production as input and produces a vector of terminals as output
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(){
+    std::ifstream myfile ("transformedgrammar.txt");
+    std::string mystring;
+    if (myfile.open()){
+        myfile >> mystring;
+        std::cout << mystring;
+    }
+    myfile.close();
+    return 0;
+}
