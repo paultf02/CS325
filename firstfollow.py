81cf5ba641a11ec379db925152aba77770b6b86e
Paul <paulumonty@gmail.com>
2023-11-06 20:29:19 +0000

    added comma to grammar in params, wrote python file for firstfollow

--- firstfollow.py
+++ firstfollow.py
@@ -1,2 +1,21 @@
+lhslist = [] # list of nonterminals in order of production
+terminals = []
 with open("transformedgrammar3.txt", "r") as fin:
-    
+    lines = fin.readlines()
+rhslist = []
+for l in lines:
+    a = l.rstrip().split(" -> ")
+    production, fullrhs = a
+    rhs = [term.split(" ") for term in fullrhs.split(" | ")]
+    # rhslist = [list of rhs]
+    # rhs = [list of or sections]
+    # or_sections = [list of words]
+    lhslist.append(production)
+    rhslist.append(rhs)
+#print(nonterminals)
+
+
+# for i in range(len(rhslist)):
+#     print(f"{lhslist[i]} is the production, rhs is a {type(rhslist[i])} it has {len(rhslist[i])} options")
+
+print(rhslist[29][1])
