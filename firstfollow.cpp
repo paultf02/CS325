986c73da93685ef8cccb99911c83268dea58248f
Paul Francis <paulumonty@gmail.com>
2023-11-06 17:43:19 +0000

    x

--- firstfollow.cpp
+++ firstfollow.cpp
@@ -4,10 +4,23 @@
 // we want production class. 
 // list of productions
 // first is a function that takes a production as input and produces a vector of terminals as output
+// the function operating on a production uses what aspect? it finds the unions
 
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+
+class Production{
+    std::string head;
+    std::vector<std::string> disjuncs;
+}
+
+std::vector<std::string> first(Production p){
+    std::vector<std::string> firstset;
+    
+    return firstset;
+}
 
 int main(){
     std::ifstream myfile ("transformedgrammar.txt");
@@ -15,7 +28,7 @@ int main(){
     if (myfile.is_open()){
         while (myfile.good()){
             myfile >> mystring;
-            std::cout << mystring;
+            std::cout << mystring << '\n';
         }
     }
     myfile.close();
